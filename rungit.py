00de3587aa2a012ce65573442f2545c9fea1c073
mukesh.7755@gmail.com <mukesh.7755@gmail.com>
2023-01-08 22:14:02 +0530

    adding file

--- rungit.py
+++ rungit.py
@@ -1,4 +1,10 @@
 import os
+
+
+
+
+# Change the current working directory to "/home/user/documents"
+os.chdir("C:/Users/Mukesh/OneDrive/Desktop/python/autopy/project-automation1")
 os.system("git add .")
 
 os.system("git commit -m \"adding file\" ")
